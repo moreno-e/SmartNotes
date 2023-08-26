c7de1aeffa536960ba68cdd2191674aaa4f02b1c
Eric Moreno <ericmoreno@Erics-MBP.attlocal.net>
2023-08-26 14:28:36 -0400

    added authorize route

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -18,3 +19,7 @@ def home(request):
 
     # args(original request, name of the template, empty brackets)
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorize(request):
+    return render(request, 'home/authorize.html', {})
