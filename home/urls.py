86f8f281dde42228e550173f453af4d2eb80e70b
Eric Moreno <ericmoreno@Erics-MBP.attlocal.net>
2023-08-27 13:19:23 -0400

    switch our home views form function based to class based

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
-    path('authorized', views.authorize)
+
+    # we base the home funciton here
+    # path('home', views.home),
+
+    # passing the HomeView Class
+    # need to class the as_view()
+    path('home', views.HomeView.as_view())
+
+    # path('authorized', views.authorize)
+    path('authorized', views.AuthorizedView.as_view())
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+# removed since switching from function to class based view
+# from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-def home(request):
+# CLASS BASED VIEW
+class HomeView(TemplateView):
+    template_name 'home/welcome.html'
+    extra_context = {'today'; datetime.today()}
 
-    # return HttpResponse('Hello World')
 
-    # Instead of using HTTPResponse, use the function render
-    # from Django shortcuts
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
 
-    # All though we are writing an HTML page, Django is using a template 
-    # framework to create the final HTML page in the browser
 
-    # {} will allow us to pass down information from the view to the template
+# FUNCTION BASED VIEW
+# def home(request):
 
-    # args(original request, name of the template, empty brackets)
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
+#     # return HttpResponse('Hello World')
 
-@login_required(login_url='/admin')
-def authorize(request):
-    return render(request, 'home/authorize.html', {})
+#     # Instead of using HTTPResponse, use the function render
+#     # from Django shortcuts
+
+#     # All though we are writing an HTML page, Django is using a template 
+#     # framework to create the final HTML page in the browser
+
+#     # {} will allow us to pass down information from the view to the template
+
+#     # args(original request, name of the template, empty brackets)
+#     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+# FUNCTION BASED VIEW
+# @login_required(login_url='/admin')
+# def authorize(request):
+#     return render(request, 'home/authorize.html', {})
