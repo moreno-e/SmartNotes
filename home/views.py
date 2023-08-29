a3f87c6a22d165ed7c3c27ed6ca28663e77d8b7a
Eric Moreno <ericmoreno@Erics-MBP.attlocal.net>
2023-08-28 23:02:18 -0400

    added signup page

--- home/views.py
+++ home/views.py
@@ -4,8 +4,25 @@ from datetime import datetime
 # removed since switching from function to class based view
 # from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    # Need to add below
+    # Need to make sure that only people that are not logged in can access he signup page
+    # can do this by overriding the get method and redirecting the user if they are already logged in
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
